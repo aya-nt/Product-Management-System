e02892e28863c89dd3dae37eaa008cbd0fb6db8f
aya-niati <ayaterrahmane.niati@gmail.com>
2025-12-20 20:37:14 +0100

    Update Vercel configuration

--- config/settings.py
+++ config/settings.py
@@ -40,9 +40,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']  # For testing, but restrict in production
+DEBUG = False
+
 
 
 # Application definition
@@ -133,7 +135,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
